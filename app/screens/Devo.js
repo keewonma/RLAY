09202e5aa2b6847e98ade681626d6eee9e01f4fb
Brent <bingr001@gmail.com>
2017-10-31 21:17:37 -0700

    Added questions

--- app/screens/Devo.js
+++ app/screens/Devo.js
@@ -11,7 +11,7 @@ export default class UselessTextInput extends Component {
     return (
     <View style={{flex: 1, padding: 20}}>
       <Text
-      style={{fontSize: 20,
+        style={{fontSize: 30,
         fontWeight: 'bold',
         textAlign: 'center',
         flexDirection: 'row',
@@ -20,6 +20,22 @@ export default class UselessTextInput extends Component {
       >
           Devotional Questions
       </Text>
+      <Text
+        style={{fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',}}
+      >
+        {"\n"}Day 1: Read Psalm 27{"\n"}
+      </Text>
+      <Text>
+      1. What stood out to you about this Psalm?{"\n"}
+      2. When was a time that you were afraid?{"\n"}
+      3. Why do you think David wrote this Psalm?{"\n"}
+      </Text>
+
       <TextInput
         style={{
           height: 100,
@@ -35,6 +51,41 @@ export default class UselessTextInput extends Component {
         onChangeText={(text) => this.setState({text})}
         value={this.state.text}
       />
+
+
+      <Text
+        style={{fontSize: 20,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',}}
+          >
+          {"\n"}Day 2: Read Exodus 1{"\n"}
+          </Text>
+          <Text>
+          1. What stood out to you about this chapter?{"\n"}
+          2. What do you think it meant that the midwives feared God?{"\n"}
+          3. What did the lack of a fear of God cause Pharaoah to do?{"\n"}
+          4. What did the fear of God cause the midwives to do? (Reread verse 17 for clues){"\n"}
+      </Text>
+      <Text
+        style={{fontSize: 20,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',}}
+          >
+          {"\n"}Day 3: Read 1 Peter 5:6-7{"\n"}
+          </Text>
+          <Text>
+          1. What stood out to you about this chapter?{"\n"}
+          2. How does fearing God make us humble?{"\n"}
+          3. How do you deal with your fears?{"\n"}
+          4. The fear of the lord cast out all fear so what should we do with those fears?{"\n"}
+      </Text>
+
     </View>
     );
   }
